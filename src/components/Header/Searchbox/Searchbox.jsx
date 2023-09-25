16020d5084e68260faa62cabffb4bd7faba16f28
sovonbd <sovonsdp@gmail.com>
2023-09-25 18:26:17 -0500

    added the search functionality

--- src/components/Header/Searchbox/Searchbox.jsx
+++ src/components/Header/Searchbox/Searchbox.jsx
@@ -1,19 +1,39 @@
-const Searchbox = () => {
+import { useState } from "react";
+
+const Searchbox = ({ setSearch }) => {
+  const [search, setSearchValue] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(search);
+    setSearchValue("");
+  };
+
+  const handleSearchText = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className="px-6 py-4 lg:py-28">
       <h1 className="text-[#0B0B0B] font-bold text-3xl lg:text-5xl text-center">I Grow By Helping People In Need</h1>
       <div className="flex justify-center items-center my-10">
-        <input
-          className="bg-white p-3 border border-[#DEDEDE] rounded-lg rounded-r-none w-[380px] focus:outline-[#FF444A] focus:border-none "
-          type="search"
-          name=""
-          id=""
-          placeholder="Search here..."
-        />
-        <button className="bg-[#FF444A] border-none px-7 rounded-l-none rounded-lg text-white font-semibold btn hover:bg-[#FF444A] focus:bg-[#FF444A]">Search</button>
+        <form onSubmit={handleSearch} className="flex items-center">
+          <input
+            onChange={handleSearchText}
+            value={search}
+            className="bg-white p-3 border border-[#DEDEDE] rounded-lg rounded-r-none w-[380px] focus:outline-[#FF444A] focus:border-none "
+            type="search"
+            name="name"
+            id=""
+            placeholder="Search here..."
+          />
+          <input type="submit" value="Search" className="bg-[#FF444A] border-none px-7 rounded-l-none rounded-lg text-white font-semibold btn hover:bg-[#FF444A] focus:bg-[#FF444A]" />
+        </form>
       </div>
+      
     </div>
   );
 };
+
 
 export default Searchbox;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,31 +2,26 @@ import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import Searchbox from "../../components/Header/Searchbox/Searchbox";
 import Card from "../../components/Card/Card";
+import { useState } from "react";
 
 const Home = () => {
   const cards = useLoaderData();
-  // console.log(cards);
+  const [search, setSearch] = useState();
+
+  const filteredCards = cards.filter((card) => card.category === search);
+
   return (
     <div className="">
       <div className="relative z-10">
-        <Searchbox></Searchbox>
+        <Searchbox setSearch={setSearch}></Searchbox>
       </div>
 
       <div className="absolute w-full top-24 opacity-25">
         <Banner></Banner>
       </div>
-      {/* <div className="relative">
-        <Searchbox></Searchbox>
-        <div className="absolute w-full -top-16 opacity-25">
-          <Banner></Banner>
-          
-        </div>
-      </div> */}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-10 m-10 container mx-auto px-6 lg:px-32">
-        {cards.map((card) => (
-          <Card key={card.id} card={card}></Card>
-        ))}
+        {filteredCards.length > 0 ? filteredCards.map((card) => <Card key={card.id} card={card}></Card>) : cards.map((card) => <Card key={card.id} card={card}></Card>)}
       </div>
     </div>
   );
